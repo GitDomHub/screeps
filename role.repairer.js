8e17f9dfba8b3e55be720c93a8144ac49bd379b4
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-06 22:03:14 +0800

    fixing repairer
    temporarily disabling the "repair lowest hits" option

--- role.repairer.js
+++ role.repairer.js
@@ -12,19 +12,23 @@ var roleRepairer = {
 
         if(creep.memory.repairing) {
             // 2Do: go for structure that has lowest hits
-            var repairneeded = creep.room.find(FIND_STRUCTURES,
+            var structuresNeedingRepair = creep.room.find(FIND_STRUCTURES,
                 {filter: (s) => s.hits < s.hitsMax * 0.5 && 
                                         s.hits < 350000});
-            if (repairneeded.length > 0) {
+            if (structuresNeedingRepair.length > 0) {
                 // find structure with lowest hitpoints
                 // 2Do: right now if there is a rampart with 270k hitpoints, then the repairer will move there. 
                 //      once it hits the same amount of hits as other ramparts, the creep will leave.
                 //      If the creep is right now already a structure that has lower than threshold hitpoints, MAYBE: first repair that one
                 //      only if there is a hostile in room, go for lowest structure, because that one might be under attack
 
-                var lowestHitsStructure = _.min(repairneeded, "hits");
+                /*var lowestHitsStructure = _.min(structuresNeedingRepair, "hits");
                 if(creep.repair(lowestHitsStructure) == ERR_NOT_IN_RANGE){
                     creep.moveTo(lowestHitsStructure, {visualizePathStyle: {stroke: '#00ff00'}});
+                }*/
+                // so far just go to the one structure that is first in list
+                if(creep.repair(structuresNeedingRepair[0]) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(structuresNeedingRepair[0], {visualizePathStyle: {stroke: '#00ff00'}});
                 }
                 else {
                     creep.repair(lowestHitsStructure);
