23c42d2f9511c981c6bec7e43e70756e3a3d96c2
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-06 21:51:19 +0800

    clean up repair
    idea on how to repair more efficiently

--- role.repairer.js
+++ role.repairer.js
@@ -9,10 +9,19 @@ var roleRepairer = {
             creep.memory.repairing = true;
             creep.say('⚙ repair');
         }
+
         if(creep.memory.repairing) {
             // 2Do: go for structure that has lowest hits
-            var repairneeded = creep.room.find(FIND_STRUCTURES,{filter: (structure) => structure.hits < structure.hitsMax * 0.5 && structure.hits < 300000});
+            var repairneeded = creep.room.find(FIND_STRUCTURES,
+                {filter: (s) => s.hits < s.hitsMax * 0.5 && 
+                                        s.hits < 300000});
             if (repairneeded.length > 0) {
+                // find structure with lowest hitpoints
+                // 2Do: right now if there is a rampart with 270k hitpoints, then the repairer will move there. 
+                //      once it hits the same amount of hits as other ramparts, the creep will leave.
+                //      If the creep is right now already a structure that has lower than threshold hitpoints, MAYBE: first repair that one
+                //      only if there is a hostile in room, go for lowest structure, because that one might be under attack
+                
                 var lowestHitsStructure = _.min(repairneeded, "hits");
                 if(creep.repair(lowestHitsStructure) == ERR_NOT_IN_RANGE){
                     creep.moveTo(lowestHitsStructure, {visualizePathStyle: {stroke: '#00ff00'}});
