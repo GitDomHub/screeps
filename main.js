c481b5d2d463b98fbcb9d2e7508a6fbf8fb3a721
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-07 23:20:30 +0800

    again

--- main.js
+++ main.js
@@ -212,8 +212,8 @@ module.exports.loop = function () {
     
     
     // manuelle überschreibung von creeps
-    Game.creeps['ð¨ Builder23382990'].memory.role = 'courier';
-    console.log('ð¨ Builder23382990 hat die Rolle: ' + Game.creeps['ð¨ Builder23382990'].memory.role);
+    Game.creeps['🔨 Builder23382990'].memory.role = 'courier';
+    console.log('🔨 Builder23382990 hat die Rolle: ' + Game.creeps['ð¨ Builder23382990'].memory.role);
     
     //console.log(Game.creeps['Repairer23189902'].owner.username);
     //Game.creeps['💎 Miner23242361'].memory.servingContainer = '5a437304ea2d2626ee07360f';
