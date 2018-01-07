403defef6744562d18000e4ebf3c45d4323e5318
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-07 16:06:48 +0800

    increase temporarily

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ module.exports.loop = function () {
     var minMiners                       = 2;    // 2Do: make enough miners as containers we have
     var minRepairers                    = 0;    // cheaper than tower repairing things
     var minUpgraders                    = 1;    // 2Do: only spawn more/bigger Upgraders than harvesters enough; only spawn new ones if overall energy amount is over certain number
-    var minBuilders                     = 2;    // 2Do only make builders when construction sites in room. 
+    var minBuilders                     = 4;    // 2Do only make builders when construction sites in room. 
     var minDefenders                    = 0;    // 2Do: 1 always on hand. Only spawn rest if hostile in room.
                                                 // Just produce a new one to be ready when old one dies
     
