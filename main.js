f17b6a82d5b35d8e73235c6dc565db3e16df26d2
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-10 21:25:03 +0800

    hits to memory

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ or changes will be lost
 //Global vars (maybe put into separate file?)
 require('vars.global');
 var repairUntilHitsEqual  = 450000; // maybe put this into memory?  
-if (!Memory.repairUntilHitsEqual) {
+if (!Memory.globals.repairUntilHitsEqual) {
     Memory.globals.repairUntilHitsEqual = repairUntilHitsEqual;
 }
 
