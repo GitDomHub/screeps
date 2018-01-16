26b59223102049558d1d8da160cd24b0a1ec9b89
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-16 18:48:04 +0800

    test

--- vars.global.js
+++ vars.global.js
@@ -56,7 +56,8 @@ Memory.rooms = Object.keys(Game.rooms);
  		                                         {filter: (s) => s.hits < s.hitsMax * 0.5 && 
  		                                             s.hits < Memory.room1.repairUntil}); 
 console.log(damage);
- var testvar = _.map(damage, 'id, hits');
+ // var testvar = _.map(damage, 'id');
+ var testvar = _(damage).map(s => {id:s.id,hits:s.hits};
 console.log(testvar);
 
 // for (s of Memory.damagedStructuresR1) {
