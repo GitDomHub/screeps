05c0250aed1b9b5d0e079c77bef209000d7c04b6
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-19 00:23:32 +0800

    debugging

--- role.upgrader.js
+++ role.upgrader.js
@@ -17,16 +17,16 @@ var roleUpgrader = {
             //Upgrader 2Do: check if controller is already set in memory!            
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 // try: move by path that is set in memory: 
-                // if (creep.memory.pathToController) {
-                //     let deser = Room.deserializePath(creep.memory.pathToController)
-                //     creep.moveByPath(deser);
-                //     creep.say('path ok');
-                // }else {
+                if (creep.memory.pathToController) {
+                    let deser = Room.deserializePath(creep.memory.pathToController)
+                    creep.moveByPath(deser);
+                    creep.say('path ok');
+                }else {
                     creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});    
-                // }                
+                }                
                 // creep.moveByPath(pathToController);
                 // creep.memory.pathToStorage = pathControllerToStorage;
-                // creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
         else {
