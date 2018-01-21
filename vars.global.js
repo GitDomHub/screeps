27196b67882f8ebad7ecb801efffa042563a8012
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-21 22:45:03 +0800

    debug

--- vars.global.js
+++ vars.global.js
@@ -150,8 +150,8 @@ for (let room in Memory.rooms) {
 
 	// increase max hits for everything when we have enough energy
 	if(storage && Object.keys(Memory.rooms[room].damagedStructures).length == 0) {
-		console.log('store energy is: ' + storage.store[RESOURCE_ENERGY] + ' and min energy is: ' + Memory.rooms[room].minEnergyInStorage);
-		if (storage.store[RESOURCE_ENERGY] > Memory.rooms[room].minEnergyInStorage) {
+		console.log('store energy is: ' + storage.store[RESOURCE_ENERGY] + ' and min energy is: ' + Memory.roomOpts[room].minEnergyInStorage);
+		if (storage.store[RESOURCE_ENERGY] > Memory.roomOpts[room].minEnergyInStorage) {
 			console.log('should increase hits now!');
 			Memory.roomOpts[room].repairUntil += Memory.roomOpts[room].aboveMinEnergyIncreaseHitsBy;
 		}
