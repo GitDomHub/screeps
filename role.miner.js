6e744739b0a3d1bc21e6cddac7b4bad63acddd56
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-27 18:34:33 +0800

    debug

--- role.miner.js
+++ role.miner.js
@@ -89,7 +89,7 @@ var roleMiner = {
                     // get source object
                     let source = Game.getObjectById(creep.memory.assignedSource);
                     // look if container is close to my assigned source
-                    let isClose = source.isNearTo(container);
+                    let isClose = source.pos.isNearTo(container);
                     console.log(isClose, ' <-------------------------- container is close to source. assigning it now.');
                     creep.memory.servingContainer = containerID; 
                     console.log(creep.memory.servingContainer, ' <-------------------------- creep.memory.servingContainer');   
