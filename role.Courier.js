0f7e93973327aad97283099f1ea689025242d7ef
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-27 16:26:05 +0800

    test

--- role.Courier.js
+++ role.Courier.js
@@ -24,11 +24,11 @@ var roleCourier = {
         
         
         let Containers = Game.rooms[creep.room.name].find(FIND_STRUCTURES, { filter: (structure) => { return ((structure.structureType === STRUCTURE_CONTAINER && structure.energy >= 1000))}});
-        let Container = creep.pos.findClosestByPath(Containers);
-        let ContainerPath = creep.room.findPath(creep.pos, Container.pos)
+        let Container_var = creep.pos.findClosestByPath(Containers);
+        let ContainerPath = creep.room.findPath(creep.pos, Container_var.pos)
         console.log(Containers, "Containers");
-        console.log(Container, "Container");
-        console.log(Cantainer, "ContainerPath");
+        console.log(Container_var, "Container");
+        console.log(CantainerPath, "ContainerPath");
 
 
         
