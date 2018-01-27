87a74721442a9859edb9ae6968d2b5102dfd5cfa
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-28 01:57:20 +0800

    fix

--- role.Courier.js
+++ role.Courier.js
@@ -100,7 +100,7 @@ var roleCourier = {
                     let dropObj = Game.getObjectById(creep.memory.assignedDrop);
                     if(dropObj.amount > 100 && dropObj.resourceType == RESOURCE_ENERGY){
                         if(creep.pickup(dropObj, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(closestContainer);
+                            creep.moveTo(dropObj);
                         }    
                     }
                     
