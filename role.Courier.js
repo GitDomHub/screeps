199e2b7bb965dd285d1f0b1c9109521e2b52d583
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-28 14:29:52 +0800

    move even if energy is little

--- role.Courier.js
+++ role.Courier.js
@@ -98,7 +98,7 @@ var roleCourier = {
                 // check if creep has dropped energy resource written into memory
                 if(creep.memory.assignedDrop){
                     let dropObj = Game.getObjectById(creep.memory.assignedDrop);
-                    if(dropObj && dropObj.amount > 200 && dropObj.resourceType == RESOURCE_ENERGY){
+                    if(dropObj && dropObj.amount > 1 && dropObj.resourceType == RESOURCE_ENERGY){
                         if(creep.pickup(dropObj, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                             creep.moveTo(dropObj);
                         }    
