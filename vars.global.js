9e08d75cfb0f93873d10e895951465e070d426dc
DomBot <35095583+GitDomHub@users.noreply.github.com>
2018-01-28 12:20:41 +0800

    fix )

--- vars.global.js
+++ vars.global.js
@@ -147,7 +147,7 @@ for (let room in Memory.rooms) {
 	// make a dropoff for each a spawn (and a controller later)
 	let spawnObj = Game.rooms[room].find(FIND_MY_SPAWNS);
 	for(let singleSpawn of spawnObj) {
-		let dropOffPos = new RoomPosition((singleSpawn.pos.x, (singleSpawn.pos.y-1), room);
+		let dropOffPos = new RoomPosition((singleSpawn.pos.x), (singleSpawn.pos.y-1), room);
 		console.log(dropOffPos, ' <-------------------- roompos for dropoff');
 	} 
 
